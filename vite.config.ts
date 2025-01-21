08ae5cbb373a0333694ebe56e95c2cf20d5d71de
Kees Jansen <jansengsy@gmail.com>
2025-01-21 11:23:47 +0000

    fix: attempting to fix Netlify routing to the built widget index.html files

--- vite.config.ts
+++ vite.config.ts
@@ -8,18 +8,18 @@ export default defineConfig({
     rollupOptions: {
       input: {
         // Widgets
-        counter: path.resolve(__dirname, "src/widgets/counter/index.html"),
-        message: path.resolve(__dirname, "src/widgets/message/index.html"),
+        "counter/index": path.resolve(
+          __dirname,
+          "src/widgets/counter/index.html"
+        ),
+        "message/index": path.resolve(
+          __dirname,
+          "src/widgets/message/index.html"
+        ),
         // Examples
         "example-counter": path.resolve(__dirname, "examples/counter.html"),
         "example-message": path.resolve(__dirname, "examples/message.html"),
         "example-all": path.resolve(__dirname, "examples/all.html"),
-      },
-      output: {
-        dir: "dist",
-        entryFileNames: `assets/[name].[hash].js`,
-        chunkFileNames: `assets/[name].[hash].js`,
-        assetFileNames: `assets/[name].[hash].[ext]`,
       },
     },
   },
